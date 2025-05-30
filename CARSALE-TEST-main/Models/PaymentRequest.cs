fa715c4ae1bae350fec1ea9dbd87dd0767490a24
giang đặng <giangdang12378@gmail.com>
2025-05-30 16:39:31 +0700

    admin

--- CARSALE-TEST-main/Models/PaymentRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CARSALE.Models
-{
-    public class PaymentRequest
-    {
-        public int OrderID { get; set; }
-        public string PaymentMethod { get; set; } = string.Empty;
-        public string TransactionCode { get; set; } = string.Empty;
-    }
-}
--- CARSALE-TEST-main/Models/VerifyResetRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CARSALE.Models
-{
-    public class VerifyResetRequest
-    {
-        public string Email { get; set; } = string.Empty;
-        public string OTP { get; set; } = string.Empty;
-        public string NewPassword { get; set; } = string.Empty;
-    }
-}
